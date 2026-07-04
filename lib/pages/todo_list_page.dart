6dc0b48b2073fc19a49d776e5fb12997e23d78a9
Anderson Boeing <anderboeing@gmail.com>
2026-07-04 14:22:57 -0300

    Expanded e padding

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -3,51 +3,28 @@ import 'package:flutter/material.dart';
 class TodoListPage extends StatelessWidget {
   TodoListPage({super.key});
 
-  final TextEditingController _taskController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Lista de tarefas')),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
             children: [
-              TextField(
-                controller: _taskController,
-                decoration: InputDecoration(
-                  labelText: 'Digite uma tarefa',
-                  border: OutlineInputBorder(),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Adicione uma tarefa',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                onChanged: (value) => onChanged(value),
-                onSubmitted: (value) => login(_taskController),
               ),
-              ElevatedButton(
-                onPressed: () => login(_taskController),
-                child: Text('Adicionar tarefa'),
-              ),
+              SizedBox(width: 8),
+              ElevatedButton(onPressed: () {}, child: Text('+')),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-void login(dynamic taskController) {
-  String text = taskController.text;
-  print(text);
-  taskController.clear();
-}
-
-void onChanged(String value) {
-  // somente leitura do valor digitado no TextField, sem alterar o estado do widget
-  print('Texto digitado: $value');
-}
-
-void onSubmitted(String value) {
-  // somente leitura do valor digitado no TextField, sem alterar o estado do widget
-  print('Texto enviado: $value');
 }
